00fe9990f562a4a9e2828251b034109147ea4a00
Abhishekghosh2002 <118560412+Abhishekghosh2002@users.noreply.github.com>
2022-11-24 00:36:04 +0530

    Create Q56.java

---
+++ Q56.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q56 {
+
+	public static void main(String[] args) {
+	int a;
+	System.out.println("Enter a number");
+	for(int i=0;i<10;i++) {
+		System.out.println(i);
+	}
+	
+
+	}
+
+}
